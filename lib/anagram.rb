e25e793efd8a2ecfa5bef3e2982ffc25f07de9c9
Seth David Gyimah <esec26@gmail.com>
2022-08-24 14:41:58 +0000

    anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require "pry"
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    #select (filter) returns array that match specified condition
+    #iterate through all array elements, return each element that,
+    # when split and sorted, matches @word splitted and sorted.
+    def match(array)
+        results = []
+        array.select do |item|
+            item.split('').sort == @word.split('').sort
+        end
+    end
+
+end
+
+come = Anagram.new('come')
+binding.pry
